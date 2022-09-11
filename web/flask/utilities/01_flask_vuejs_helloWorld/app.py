3610cc586b7cdde04776c38fcb1b853d4b02c888
Nathaniel Collier <drxs3v3n@gmail.com>
2022-09-11 17:47:26 -0500

    Add some documentation

--- web/flask/utilities/01_flask_vuejs_helloWorld/app.py
+++ web/flask/utilities/01_flask_vuejs_helloWorld/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, send_from_directory
 import os
 
+# run this application with `python3 -m flask run`
+
 app = Flask(__name__)
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "www")
