2736bef185b367015abc3fd1fbb3a3574746c51b
Nathaniel Collier <drxs3v3n@gmail.com>
2023-02-25 11:50:10 -0600

    Put in a simple button click This needs to be organized better somehow

--- web/flask/htmx/01_flask_htmx_demo/controllers/general.py
+++ web/flask/htmx/01_flask_htmx_demo/controllers/general.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 general_bp = Blueprint("general_blueprint", __name__)
 
+buttonClickCounter = 0
+
 @general_bp.route("/currentTime")
 def getCurrentTime():
     now = datetime.now()
@@ -10,4 +12,12 @@ def getCurrentTime():
         <div>
             {now}
         </div>
+    '''
+
+@general_bp.route("/buttonClick1")
+def getButtonClick1():
+    global buttonClickCounter
+    buttonClickCounter += 1
+    return f'''
+        <span style='color:red;font-weight:bold;'>You clicked the button {buttonClickCounter}</span>
     '''
