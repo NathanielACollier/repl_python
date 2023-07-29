f56729f8a0d54c2e3f19763bc4d3eb06b57e42c8
Nathaniel Collier <drxs3v3n@gmail.com>
2023-07-29 15:28:37 -0500

    Some changes needed to go to React 18 + Still not working right

--- web/flask/react/react002_axiosExperiments/reactApp/app.tsx
+++ web/flask/react/react002_axiosExperiments/reactApp/app.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 
 /*
 Working on following this guide: https://reactjsexample.com/hello-react-create-a-minimalist-react-app/
 */
 
+class App extends React.Component {
+    state = { myCounter: 3 }
+    
+    onClick(){
+        this.setState({
+            myCounter: this.state.myCounter + 1
+        });
+    }
 
-function App() {
-    return <h1>hello world</h1>
+    render(){
+      return(
+        <div>
+            <h2>App</h2>
+            <div>Click count: {this.state.myCounter}</div>
+            <button onClick={this.onClick}>Click Me!</button>
+        </div>
+      );
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const domNode = document.getElementById('root');
+const root = createRoot(domNode)
+  
+root.render(<App />);
