ce217de9b2c57ccb04262a007662549be36ac96c
Nathaniel Collier <drxs3v3n@gmail.com>
2023-07-29 13:51:20 -0500

    Start a project to experiment with flask and react

---
+++ web/flask/react/react001_demo/reactApp/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+/*
+Working on following this guide: https://reactjsexample.com/hello-react-create-a-minimalist-react-app/
+*/
+
+function App() {
+    return <h1>hello world</h1>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
