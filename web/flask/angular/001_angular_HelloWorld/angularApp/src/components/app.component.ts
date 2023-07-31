1979bbaef0f8dd5c2cfbe8a75f74eb02a8363f19
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-07-31 08:53:41 -0500

    Pull in pieces of the angular default project + Finding the minimum of the project

---
+++ web/flask/angular/001_angular_HelloWorld/angularApp/src/components/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello world!</h1>
+  `,
+  styleUrls: []
+})
+export class AppComponent {
+  
+}
---
+++ web/flask/angular/001_angular_HelloWorld/angularApp/src/main.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './components/app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
