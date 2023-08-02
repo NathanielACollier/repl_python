2fc73721e297976089a9b382abc4ab6d1f976bb6
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-08-01 21:41:18 -0500

    Simplify the App component page

--- web/flask/angular/002_angular_Testing/angularApp/src/components/app.component.ts
+++ web/flask/angular/002_angular_Testing/angularApp/src/components/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello world!</h1>
+    <div>Angular Test App</div>
+    <hr />
 
     <!-- Angular Routes -->
     <router-outlet></router-outlet>
