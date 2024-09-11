78c183deacf0fdbae7ea7d6ae57631c88ca39452
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-09-11 07:01:27 -0500

    Start work on figuring out how to do MS Login in flask.   Need to figure out some python stuff before this will work

---
+++ web/flask/other/oauthLogin_Microsoft/app.py
@@ -0,0 +1,51 @@
+
+from flask import Flask, redirect, url_for, session
+from flask_oauthlib.client import OAuth
+import os
+
+# -- Run --
+# pipenv shell
+# pipenv install
+# python3 -m flask run
+
+# -- NOtes --
+# original code from: https://medium.com/@tempmailwithpassword/integrate-flask-web-apps-with-microsoft-365-login-ae171d7b2164
+
+app = Flask(__name__)
+
+oauth = OAuth(app)
+
+microsoft = oauth.remote_app(
+    'microsoft',
+    consumer_key='YOUR_APP_ID',
+    consumer_secret='YOUR_APP_SECRET',
+    request_token_params={'scope': 'User.Read'}
+    base_url='https://graph.microsoft.com/v1.0/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://login.microsoftonline.com/common/oauth2/v2.0/token',
+    authorize_url='https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+)
+@app.route('/')
+def index():
+    return '<h1>Welcome to the Flask App</h1>' + '<a href="/login">Login with Microsoft</a>'
+
+@app.route('/login')
+def login():
+    return microsoft.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/login/authorized')
+def authorized():
+    response = microsoft.authorized_response()
+    if response is None or response.get('access_token') is None:
+        return 'Access denied: reason={0} error={1}'.format(
+            request.args['error'], request.args['error_description'])
+    session['oauth_token'] = (response['access_token'], '')
+    return 'Logged in as id={0}'.format(session['oauth_token'])
+
+@microsoft.tokengetter
+def get_microsoft_oauth_token():
+    return session.get('oauth_token')
+
+if __name__ == '__main__':
+    app.run(debug=True)
