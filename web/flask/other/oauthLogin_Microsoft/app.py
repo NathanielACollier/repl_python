720f7ad655cd4d93e7efae1a918558b1728fc148
Nathaniel Collier <drxs3v3n@gmail.com>
2024-09-16 20:02:33 -0500

    Move all settings to a dictionary at the top so it's easier to debug what is happening

--- web/flask/other/oauthLogin_Microsoft/app.py
+++ web/flask/other/oauthLogin_Microsoft/app.py
@@ -15,25 +15,31 @@ app = Flask(__name__)
 
 oauth = OAuth(app)
 
+settings = {
+    "msClientId": nac_settings.local.get("TestAuthAzureAppID"),
+    "msClientSecret": nac_settings.local.get("TestAuthAzureClientSecret"),
+    "redirectUri":"http://localhost:5000/auth"
+}
+
 oauth.register(
     name='microsoft',
-    client_id=nac_settings.local.get("TestAuthAzureAppID"),
-    client_secret=nac_settings.local.get("TestAuthAzureClientSecret"),
+    client_id=settings["msClientId"],
+    client_secret=settings['msClientSecret'],
     authorize_url='https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
     authorize_params=None,
     access_token_url='https://login.microsoftonline.com/common/oauth2/v2.0/token',
     access_token_params=None,
     refresh_token_url=None,
-    redirect_uri=os.getenv('REDIRECT_URI'),
+    redirect_uri=settings['redirectUri'],
     client_kwargs={'scope': 'User.Read'}
 )
 
 
 @app.route('/')
 def index():
-    return '<h1>Welcome to the Flask App</h1>' + '<a href="/login">Login with Microsoft</a>'
+    return '<h1>Welcome to the Flask App</h1>' + '<a href="/msLogin">Login with Microsoft</a>'
 
-@app.route('/login')
+@app.route('/msLogin')
 def login():
     redirect_uri = url_for('auth', _external=True)
     return oauth.microsoft.authorize_redirect(redirect_uri)
